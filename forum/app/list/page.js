8ee9a15979a75ae1551a5935a745fe80c276a63a
CCHS-React <cchsreact@gmail.com>
2024-07-30 14:57:17 +0900

    7_30

--- forum/app/list/page.js
+++ forum/app/list/page.js
@@ -11,6 +11,7 @@ export default async function List() {
         result.map((a,i) => 
           <div className='list-item' key={i}>
             <Link href={'/detail/'+result[i]._id}><h4>{a.title}</h4></Link>
+            <Link href={'/edit/'+result[i]._id} className="list-btn">🔼</Link>
             <p>1월 1일</p>
           </div>
         )
